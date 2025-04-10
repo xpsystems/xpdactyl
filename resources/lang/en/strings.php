884b0b4369a4198883088a6a270f0366105b20e0
Fabian Ternis <151365993+Michaelninder@users.noreply.github.com>
2025-04-10 07:03:54 +0200

    Update strings.php

--- resources/lang/en/strings.php
+++ resources/lang/en/strings.php
@@ -91,5 +91,5 @@ return [
     'enable' => 'Enable',
     'disable' => 'Disable',
     'save' => 'Save',
-    'copyright' => '&copy; 2015 - :year Pterodactyl Software',
+    'copyright' => 'XPdactyl based on the &copy; Pterodactyl Software',
 ];
